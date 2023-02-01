93797a4d5082702c2a0860802f8688a4f9c356e2
barrystone <barrystone3183@gmail.com>
2023-02-01 17:49:08 +0800

    [feat] template

--- app.ts
+++ app.ts
@@ -1,15 +1,36 @@
 import koa, { Context } from 'koa';
 import koaRouter from 'koa-router';
 import json from 'koa-json';
+import path from 'path';
+import render from 'koa-ejs';
 
 const app = new koa();
 const router = new koaRouter();
+
+// Replace with DB
+const things = ['Programming', 'Sport', 'Music'];
 
 // Json prettier middleware
 app.use(json());
 
 // Simple middleware example
 // app.use(async (ctx: Context) => (ctx.body = { msg: 'Hello World' }));
+
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'layout',
+  viewExt: 'html',
+  cache: false,
+  debug: false
+});
+
+// index
+router.get('/', async (ctx) => {
+  await ctx.render('index', {
+    title: 'Things I Love:',
+    things: things
+  });
+});
 
 router.get('/test', (ctx) => (ctx.body = 'test'));
 // Router Middleware
