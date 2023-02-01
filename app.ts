4c13e4e35608a579882fc713c5c6abe5f8497b2f
barrystone <barrystone3183@gmail.com>
2023-02-01 16:53:24 +0800

    [feat] koa-router & koa-json

--- app.ts
+++ app.ts
@@ -1,8 +1,18 @@
 import koa, { Context } from 'koa';
+import koaRouter from 'koa-router';
+import json from 'koa-json';
 
 const app = new koa();
+const router = new koaRouter();
 
-// app.use(async (ctx: Context) => (ctx.body = 'Hello World'));
-app.use(async (ctx: Context) => (ctx.body = { msg: 'Hello World' }));
+// Json prettier middleware
+app.use(json());
+
+// Simple middleware example
+// app.use(async (ctx: Context) => (ctx.body = { msg: 'Hello World' }));
+
+router.get('/test', (ctx) => (ctx.body = 'test'));
+// Router Middleware
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000, () => console.log('Server started...'));
